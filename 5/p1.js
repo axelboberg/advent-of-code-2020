a7d43c6df3e6724584b9c126d2f68fe64a294a29
Axel Boberg <axel@axelboberg.se>
2020-12-06 00:42:30 +0100

    Shorten code

--- 5/p1.js
+++ 5/p1.js
@@ -5,22 +5,9 @@ const input = fs.readFileSync('./input.txt')
 
 /* --- EVERYTHING ABOVE THIS LINE IS GENERAL SETUP --- */
 
-function binToDec (input = [], zero = 0) {
-  let val = 0
-  for (let i = input.length; i >= 0; i--) {
-    if (input[i] === zero) continue
-    val += Math.pow(2, input.length - i - 1)
-  }
-  return Math.floor(val)
-}
-
 let highest = 0
 for (let seat of input) {
-  const row = seat.substring(0, 7).split(''),
-        col = seat.substr(7).split('')
-
-  const id = binToDec(row, 'F') * 8 + binToDec(col, 'L')
-  
+  const id = parseInt(seat.replace(/F|L/g, 0).replace(/B|R/g, 1), 2)
   if (highest < id) highest = id
 }
 
--- 5/p2.js
+++ 5/p2.js
@@ -5,25 +5,10 @@ const input = fs.readFileSync('./input.txt')
 
 /* --- EVERYTHING ABOVE THIS LINE IS GENERAL SETUP --- */
 
-function binToDec (input = [], zero = 0) {
-  let val = 0
-  for (let i = input.length; i >= 0; i--) {
-    if (input[i] === zero) continue
-    val += Math.pow(2, input.length - i - 1)
-  }
-  return Math.floor(val)
-}
-
 const seats = []
-for (let seat of input) {
-  const row = seat.substring(0, 7).split(''),
-        col = seat.substr(7).split('')
-
-  const id = binToDec(row, 'F') * 8 + binToDec(col, 'L')
+input.forEach(seat => {
+  const id = parseInt(seat.replace(/F|L/g, 0).replace(/B|R/g, 1), 2)
   seats[id] = id
-}
+})
 
-for (let seat in seats) {
-  const next = parseInt(seat) + 1
-  if (!seats[next]) return console.log('OUTPUT:', next)
-}
+seats.some((_, i) => (!seats[i+1] && (console.log('OUTPUT:', i+1) || 1)))
